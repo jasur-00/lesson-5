d1c83852eb9b6d1a8b63660181095ce3a240aa96
turdaliyevjasur02@gmail.com <turdaliyevjasur02@gmail.com>
2022-07-26 21:29:01 -0400

    git add

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var intro = alert("Xush kelibsiz");
+var first_name = prompt("Ismingiz nima?", "Jasur").trim();
+var last_name = prompt("Familiyangiz nima?", "Turdaliyev").trim();
+var age = prompt("Yoshingiz nechida?", "20 yoshda").trim();
+var jobs = prompt("Kasbingiz nima?", "Dasturchi").trim();
+var home = prompt("Yashash joyingiz qayer?", "Toshkent shaxri").trim();
+var alert = alert("Rahmat");
+
+console.log (`Malumotlaringiz: 
+Foydalanuvchining ism-sharifi:${first_name} ${last_name} 
+Foydalanuvchining yoshi:${age} 
+Foydalanuvchining kasbi:${jobs} 
+Foydalanuvchinig yashash joyi:${home}`);
